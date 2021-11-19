c629924f0ad0f215a53d674b7eca1c8f7c536932
Esgrid <esikahall@gmail.com>
2021-11-19 18:21:29 +0000

    week1 weekend homework solution

---
+++ src/pet_shop.py
@@ -0,0 +1,63 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, pet_sold):
+    pet_shop["admin"]["pets_sold"] += pet_sold
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    return [pet for pet in pet_shop["pets"] if pet["breed"] == breed]
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            r_pet = pet
+    pet_shop["pets"].remove(r_pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, cash):
+    customer["cash"] -= cash
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+def customer_can_afford_pet(customer, new_pet):
+    return customer["cash"] >= new_pet["price"]
+
+# def find_pet_by_name(pet_shop, name):
+#     pass
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if customer_can_afford_pet(customer, pet) and pet != None:
+        remove_pet_by_name(pet_shop, pet["name"])
+        add_pet_to_customer(customer, pet)
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        increase_pets_sold(pet_shop, 1)
+
+
